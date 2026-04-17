420af1e8e10198efc4c2fdf6aa41f40d688328aa
ljxlljjxx <ljxlljjxxs@163.com>
2026-04-17 22:13:15 +0800

    change

--- src/PowerViolenceObjects/pvc_PV_119p8.h
+++ src/PowerViolenceObjects/pvc_PV_119p8.h
@@ -32,14 +32,14 @@
 #endif
     
 
-typedef struct pvc_int128
+typedef struct pvc_PV_119p8
 {
     int64_t  _1;
     uint64_t _2;
-} pvc_int128;
+} pvc_PV_119p8;
 
-bool pvc_int128_add(pvc_int128 *a, pvc_int128 *b, pvc_int128 *restrict res);
-bool pvc_int128_neg(pvc_int128 *a);
-char *pvc_int128_tostring(pvc_int128 *a);
-int pvc_int128_format(char *restrict buffer, const char *restrict format, pvc_int128 *restrict a, ...);
-void pvc_int128_print(pvc_int128 *a);
+bool pvc_PV_119p8_add(pvc_PV_119p8 *a, pvc_PV_119p8 *b, pvc_PV_119p8 *restrict res);
+bool pvc_PV_119p8_neg(pvc_PV_119p8 *a);
+char *pvc_PV_119p8_tostring(pvc_PV_119p8 *a);
+int pvc_PV_119p8_format(char *restrict buffer, const char *restrict format, pvc_PV_119p8 *restrict a, ...);
+void pvc_PV_119p8_print(pvc_PV_119p8 *a);
