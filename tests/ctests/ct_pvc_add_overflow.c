65829200919a5622aeaee7ad069970434610ed3f
ljxlljjxx <ljxlljjxxs@163.com>
2026-04-18 20:28:08 +0800

    change

--- tests/ctests/ct_pvc_add_overflow.c
+++ tests/ctests/ct_pvc_add_overflow.c
@@ -1,10 +1,10 @@
 /* 
 Test:
-gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_PV_119p8.c -o pvc_PV_119p8-macos.o
+gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_add_overflow.c -o pvc_add_overflow-macos.o
 gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos.o
-gcc -Wall -Wextra -c tests/ctests/ct_pvc_PV_119p8.c -o ct_pvc_PV_119p8-macos.o -Isrc/PowerViolenceObjects
-gcc pvc_PV_119p8-macos.o ct_pvc_PV_119p8-macos.o pvc_defines-macos.o -o ct_pvc_PV_119p8-macos
-./ct_pvc_PV_119p8-macos
+gcc -Wall -Wextra -c tests/ctests/ct_pvc_add_overflow.c -o ct_pvc_add_overflow-macos.o -Isrc/PowerViolenceObjects
+gcc pvc_add_overflow-macos.o ct_pvc_add_overflow-macos.o pvc_defines-macos.o -o ct_pvc_add_overflow-macos
+./ct_pvc_add_overflow-macos
 */
 
 #include "../main.h"
