8a3b0ee462c35adede2357befedb2667d9ced090
ljxlljjxx <ljxlljjxxs@163.com>
2026-04-26 12:09:22 +0800

    fix z_test.sh

--- src/PowerViolenceObjects/pvc_PV_119p8.h
+++ src/PowerViolenceObjects/pvc_PV_119p8.h
@@ -10,5 +10,5 @@ void pvc_PV_119p8_set(pvc_PV_119p8 *res, __int128_t *a);
 bool pvc_PV_119p8_add(pvc_PV_119p8 *a, pvc_PV_119p8 *b, pvc_PV_119p8 *restrict res);
 bool pvc_PV_119p8_neg(pvc_PV_119p8 *a);
 char *pvc_PV_119p8_tostring(pvc_PV_119p8 *a);
-int pvc_PV_119p8_format(char *restrict buffer, const char *restrict format, pvc_PV_119p8 *restrict a, ...);
+int pvc_PV_119p8_format(char *restrict buffer, const char *restrict format, pvc_PV_119p8 *restrict a, int *format_length, ...);
 int pvc_PV_119p8_print(pvc_PV_119p8 *a);
--- useless/z_test.sh
+++ useless/z_test.sh
@@ -8,10 +8,13 @@ echo 'C Test pvc_add_overflow'
 gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos.o
 gcc -Wall -Wextra -c tests/ctests/ct_pvc_add_overflow.c -o ct_pvc_add_overflow-macos.o -Isrc/PowerViolenceObjects
 gcc ct_pvc_add_overflow-macos.o pvc_defines-macos.o -o ct_pvc_add_overflow-macos
-./ct_pvc_add_overflow-macos
-
-if [ $? -eq 1 ];
-    then return_val=1
+if [ $? -eq 1 ]; then
+    return_val=1;
+else
+    ./ct_pvc_add_overflow-macos
+    if [ $? -eq 1 ]; then
+        return_val=1;
+    fi
 fi
 
 
@@ -21,10 +24,13 @@ gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_PV_119p8.c -o pvc_PV_119p8-mac
 gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos.o
 gcc -Wall -Wextra -c tests/ctests/ct_pvc_PV_119p8.c -o ct_pvc_PV_119p8-macos.o -Isrc/PowerViolenceObjects
 gcc pvc_PV_119p8-macos.o ct_pvc_PV_119p8-macos.o pvc_defines-macos.o -o ct_pvc_PV_119p8-macos
-./ct_pvc_PV_119p8-macos
-
 if [ $? -eq 1 ]; then
-    return_val=1
+    return_val=1;
+else
+    ./ct_pvc_PV_119p8-macos
+    if [ $? -eq 1 ]; then
+        return_val=1;
+    fi
 fi
 
 
@@ -34,11 +40,15 @@ gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_PV_57p8.c -o pvc_PV_57p8-macos
 gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos.o
 gcc -Wall -Wextra -c tests/ctests/ct_pvc_PV_57p8.c -o ct_pvc_PV_57p8-macos.o -Isrc/PowerViolenceObjects
 gcc pvc_PV_57p8-macos.o ct_pvc_PV_57p8-macos.o pvc_defines-macos.o -o ct_pvc_PV_57p8-macos
-./ct_pvc_PV_57p8-macos
-
 if [ $? -eq 1 ]; then
-    return_val=1
+    return_val=1;
+else
+    ./ct_pvc_PV_57p8-macos
+    if [ $? -eq 1 ]; then
+        return_val=1;
+    fi
 fi
+
 
 
 if [ $return_val -eq 1 ]; then
