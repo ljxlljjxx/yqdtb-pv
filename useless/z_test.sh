3c0452919fc94ad3852a755fefcffebc3f2ca78f
ljxlljjxx <ljxlljjxxs@163.com>
2026-04-26 11:42:09 +0800

    write docs for pvc_PV_119p8_format

--- useless/z_test.sh
+++ useless/z_test.sh
@@ -1,4 +1,5 @@
 PV='/Users/ljx/Desktop/P/github/PowerViolence'
+return_val=0
 cd $PV
 
 echo 'C Test pvc_add_overflow'
@@ -7,6 +8,10 @@ gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos
 gcc -Wall -Wextra -c tests/ctests/ct_pvc_add_overflow.c -o ct_pvc_add_overflow-macos.o -Isrc/PowerViolenceObjects
 gcc ct_pvc_add_overflow-macos.o pvc_defines-macos.o -o ct_pvc_add_overflow-macos
 ./ct_pvc_add_overflow-macos
+
+if [ $? -eq 1 ]; then
+    return_val=1
+fi
 
 
 echo 'C Test pvc_PV_119p8.c'
@@ -17,6 +22,10 @@ gcc -Wall -Wextra -c tests/ctests/ct_pvc_PV_119p8.c -o ct_pvc_PV_119p8-macos.o -
 gcc pvc_PV_119p8-macos.o ct_pvc_PV_119p8-macos.o pvc_defines-macos.o -o ct_pvc_PV_119p8-macos
 ./ct_pvc_PV_119p8-macos
 
+if [ $? -eq 1 ]; then
+    return_val=1
+fi
+
 
 echo 'C Test pvc_PV_57p8.c'
 
@@ -25,3 +34,12 @@ gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos
 gcc -Wall -Wextra -c tests/ctests/ct_pvc_PV_57p8.c -o ct_pvc_PV_57p8-macos.o -Isrc/PowerViolenceObjects
 gcc pvc_PV_57p8-macos.o ct_pvc_PV_57p8-macos.o pvc_defines-macos.o -o ct_pvc_PV_57p8-macos
 ./ct_pvc_PV_57p8-macos
+
+if [ $? -eq 1 ]; then
+    return_val=1
+fi
+
+
+if [ $return_val -eq 1 ]; then
+    exit 1
+fi
