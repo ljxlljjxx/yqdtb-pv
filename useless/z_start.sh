6857473f944d6cd456bebb832fcef230a455f105
ljxlljjxx <ljxlljjxxs@163.com>
2026-04-26 11:10:15 +0800

    write test for pvc_PV_119p8_add

--- useless/z_start.sh
+++ useless/z_start.sh
@@ -10,8 +10,8 @@ fi
 python3 /Users/ljx/Desktop/P/github/PdfToImg/MyPdfToImg.py
 echo '规则文件图片生成完成'
 
-$PV/z_test.sh
+$PV/useless/z_test.sh
 echo '测试完成'
 
-$PV/z_clean.sh
+$PV/useless/z_clean.sh
 echo '清理'
