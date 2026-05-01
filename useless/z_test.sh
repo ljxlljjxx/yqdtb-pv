5aa05e3d280c7351deca838b0ebe912962293ebf
ljxlljjxx <ljxlljjxxs@163.com>
2026-05-01 11:15:10 +0800

    fix z_test.sh

--- useless/z_test.sh
+++ useless/z_test.sh
@@ -8,11 +8,11 @@ echo 'C Test pvc_add_overflow'
 gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos.o
 gcc -Wall -Wextra -c tests/ctests/ct_pvc_add_overflow.c -o ct_pvc_add_overflow-macos.o -Isrc/PowerViolenceObjects
 gcc ct_pvc_add_overflow-macos.o pvc_defines-macos.o -o ct_pvc_add_overflow-macos
-if [ $? -eq 1 ]; then
+if [ $? -ge 0 ]; then
     return_val=1;
 else
     ./ct_pvc_add_overflow-macos
-    if [ $? -eq 1 ]; then
+    if [ $? -ge 0 ]; then
         return_val=1;
     fi
 fi
@@ -24,11 +24,11 @@ gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_PV_119p8.c -o pvc_PV_119p8-mac
 gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos.o
 gcc -Wall -Wextra -c tests/ctests/ct_pvc_PV_119p8.c -o ct_pvc_PV_119p8-macos.o -Isrc/PowerViolenceObjects
 gcc pvc_PV_119p8-macos.o ct_pvc_PV_119p8-macos.o pvc_defines-macos.o -o ct_pvc_PV_119p8-macos
-if [ $? -eq 1 ]; then
+if [ $? -ge 0 ]; then
     return_val=1;
 else
     ./ct_pvc_PV_119p8-macos
-    if [ $? -eq 1 ]; then
+    if [ $? -ge 0 ]; then
         return_val=1;
     fi
 fi
@@ -40,11 +40,11 @@ gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_PV_55p8.c -o pvc_PV_55p8-macos
 gcc -Wall -Wextra -c src/PowerViolenceObjects/pvc_defines.c -o pvc_defines-macos.o
 gcc -Wall -Wextra -c tests/ctests/ct_pvc_PV_55p8.c -o ct_pvc_PV_55p8-macos.o -Isrc/PowerViolenceObjects
 gcc pvc_PV_55p8-macos.o ct_pvc_PV_55p8-macos.o pvc_defines-macos.o -o ct_pvc_PV_55p8-macos
-if [ $? -eq 1 ]; then
+if [ $? -ge 0 ]; then
     return_val=1;
 else
     ./ct_pvc_PV_55p8-macos
-    if [ $? -eq 1 ]; then
+    if [ $? -ge 0 ]; then
         return_val=1;
     fi
 fi
@@ -52,6 +52,5 @@ fi
 
 
 if [ $return_val -eq 1 ]; then
-    echo '???????????????????????'
     exit 1
 fi
