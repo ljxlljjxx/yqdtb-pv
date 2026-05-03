325e703eebb3c460dfc1525c721ac7cec2e7c945
ljxlljjxx <ljxlljjxxs@163.com>
2026-05-03 12:27:22 +0800

    add tests for PVOs

--- tests/ctests/test_PV_num.py
+++ tests/ctests/test_PV_num.py
@@ -2,6 +2,10 @@ import unittest
 from PowerViolenceObjects import PV_num
 
 class TestPv_num(unittest.TestCase):
+    def test_PowerViolenceObjects(self):
+        import PowerViolenceObjects as PVOs
+        self.assertEqual(PVOs.__version__, '2.2.4')
+
     def test_init(self):
         PV_num()
 
