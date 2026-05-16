01fd713350a3e5d43951506234f3f87990b452db
ljxlljjxx <ljxlljjxxs@163.com>
2026-05-16 19:26:21 +0800

    update version

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 
-version = '2.4.0'
+version = '2.5.0'
+extra_compile_args = ['-Wformat=2', '-Wconversion', '-Werror=return-type', '-Wshadow', '-Wnull-dereference', '-Wpointer-arith', '-Wfloat-equal', '-Wunreachable-code', '-Wswitch-default', '-Wswitch-enum', '-Wall', '-Wextra', '-Wno-unused-parameter', '-Werror']
 
 setuptools.setup(
     packages=['PowerViolenceObjects'],
@@ -11,7 +12,7 @@ setuptools.setup(
             sources=[
                 'src/PowerViolenceObjects/py_PV_num.c',
             ],
-            extra_compile_args=['-Wformat=2', '-Wconversion', '-Werror=return-type', '-Wshadow', '-Wnull-dereference', '-Wpointer-arith', '-Wfloat-equal', '-Wunreachable-code', '-Wswitch-default', '-Wswitch-enum', '-Wall', '-Wextra', '-Wno-unused-parameter', '-Werror'],
+            extra_compile_args=extra_compile_args,
         ),
         setuptools.Extension(
             'PowerViolenceObjects.pv_55p8',
@@ -20,7 +21,7 @@ setuptools.setup(
                 'src/PowerViolenceObjects/pvc_PV_55p8.c',
                 'src/PowerViolenceObjects/pvc_defines.c',
             ],
-            extra_compile_args=['-Wformat=2', '-Wconversion', '-Werror=return-type', '-Wshadow', '-Wnull-dereference', '-Wpointer-arith', '-Wfloat-equal', '-Wunreachable-code', '-Wswitch-default', '-Wswitch-enum', '-Wall', '-Wextra', '-Wno-unused-parameter', '-Werror'],
+            extra_compile_args=extra_compile_args,
         ),
     ],
     version=version
