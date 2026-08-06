95d7140636206e557ea76203a733c97e7fdb7d7b
ljxlljjxx <ljxlljjxxs@163.com>
2026-08-06 11:04:19 +0800

    add-github-actions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 import sys
 
-version = "3.1.100"
+version = "3.1.101"
 DEBUG = False
 
 if DEBUG:
--- src/PowerViolenceObjects/__init__.py
+++ src/PowerViolenceObjects/__init__.py
@@ -25,7 +25,7 @@ PowerViolenceObjects:
 |_  PV_struct
 """
 
-__version__ = "3.1.100"
+__version__ = "3.1.101"
 
 from typing import Union
 from .pv_str import PV_str  # type: ignore
