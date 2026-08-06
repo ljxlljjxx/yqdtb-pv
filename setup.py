d04383db1955c3a7f0b0c8559808755b143cfc06
ljxlljjxx <ljxlljjxxs@163.com>
2026-08-06 11:55:17 +0800

    fix Self don't support in some version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 import sys
 
-version = "3.1.101"
+version = "3.1.105"
 DEBUG = False
 
 if DEBUG:
--- src/PowerViolenceObjects/pv_binary.py
+++ src/PowerViolenceObjects/pv_binary.py
@@ -1,22 +1,27 @@
-from typing import Self
-
-
 class PV_binary:
-    def __init__(self, arg: Self | int | bytes):
+    def __init__(self, arg: 'PV_binary' | int | bytes):
         if isinstance(arg, PV_binary):
             self.size: int = arg.size
-            self.data: bytearray = arg.data.copy()
+            self._data: bytearray = arg._data.copy()
         elif isinstance(arg, int):
             if arg < 0:
                 raise ValueError('do not allow size < 0')
             self.size: int = arg
-            self.data: bytearray = bytearray(arg)
+            self._data: bytearray = bytearray(arg)
         elif isinstance(arg, bytes):
             self.size: int = len(arg)
-            self.data: bytearray = bytearray(arg)
+            self._data: bytearray = bytearray(arg)
         else:
             return TypeError('arg must be PV_binary or int or bytes')
         
+    @property
+    def data(self) -> bytes:
+        return bytes(self._data)
+    
+    @data.setter
+    def data(self, value: bytes):
+        self._data = bytearray(value)
+        
     def __repr__(self) -> str:
-        return f'<PV_binary object at {id(self)}, size = {self.size}>: {list(self.data)}'
+        return f'<PV_binary object at {id(self)}, size = {self.size}>: {list(self._data)}'
     
